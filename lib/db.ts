9ab999efeedaea6ae64323645417fd93e68c7a11
jdcorderor <jdcorderor@gmail.com>
2025-06-05 23:25:57 -0400

    Changed db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { Pool } from "pg";
+import { Pool } from "pg"
 
 declare global {
     // eslint-disable-next-line no-var
@@ -10,7 +10,7 @@ let pool: Pool;
 if (!global.pgPool) {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL, // Use DATABASE_URL from environment variables
-        ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false, // Use SSL in production.
+        ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false, // Use SSL in production
     });
     global.pgPool = pool;
 } else {
